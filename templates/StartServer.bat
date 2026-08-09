5de06f71c0bb55409b38c02065f69986473f3af9
cybertek67 <mullen67@gmail.com>
2026-08-09 18:55:56 -0500

    Add map-aware server template

---
+++ templates/StartServer.bat
@@ -0,0 +1,7 @@
+@echo off
+pushd "%~dp0"
+if not defined SOULMASK_MAP set "SOULMASK_MAP=Level01_Main"
+start WSServer.exe %SOULMASK_MAP% -server %* -log -UTF8Output -MULTIHOME=0.0.0.0 -EchoPort=18888 -forcepassthrough
+popd
+exit /B
+
